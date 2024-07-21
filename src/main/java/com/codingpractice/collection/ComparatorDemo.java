405a7e657bc3b3b37c51a33359fb122bb5c523cd
Sonali <sonalisinghblore@gmail.com>
2024-07-21 22:56:13 +0530

    Comparator Demo: modification, if id is same, then sort based on name.

--- src/main/java/com/codingpractice/collection/ComparatorDemo.java
+++ src/main/java/com/codingpractice/collection/ComparatorDemo.java
@@ -17,10 +17,12 @@ public class ComparatorDemo {
             Student s1 = new Student(1, "Sonu");
             Student s2 = new Student(5, "Sunny");
             Student s3 = new Student(2, "Nishanth");
+            Student s4 = new Student(2,"Naseer");
             List<Student> students = new ArrayList<>();
             students.add(s1);
             students.add(s2);
             students.add(s3);
+            students.add(s4);
 
             //printing before sorting
             System.out.println("before sorting: " + students);
@@ -32,7 +34,7 @@ public class ComparatorDemo {
             Collections.sort(students, new IdComparator());
 
             // after sorting based on id
-            System.out.println("after sorting based on id: " + students);
+            System.out.println("after sorting based on id, but if id is same, then sort based on name: " + students);
         }
     }
 }
