0a2e34dc39bb814a9f2e19991a21de4f7e1d5f0a
Sonali <sonalisinghblore@gmail.com>
2024-07-23 11:54:54 +0530

    Concurrent modification exception fix by using concurrent Hash Map..

--- src/main/java/com/codingpractice/FailFastMap.java
+++ src/main/java/com/codingpractice/FailFastMap.java
@@ -3,10 +3,12 @@ package com.codingpractice;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class FailFastMap {
     public static void main(String[] args) {
-        Map<Integer, String> map=new HashMap<>();
+        Map<Integer, String> map=new ConcurrentHashMap<>();
         map.put(1,"one");
         map.put(2,"two");
 
